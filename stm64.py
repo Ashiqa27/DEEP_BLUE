fb03c4b6e796ccefd470e4e28b73d6f6abf96713
Ashiqa27 <87598232+Ashiqa27@users.noreply.github.com>
2023-03-30 20:39:29 +0530

    Update stm64.py

--- stm64.py
+++ stm64.py
@@ -15,6 +15,7 @@ from docx import Document
 from docx.shared import Pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from moviepy.video.io.VideoFileClip import VideoFileClip
+import threading
 
 # Set the title of the app
 st.title("Video Meeting Summarizer")
@@ -44,71 +45,74 @@ if uploaded_file is not None:
     if recipient_email:
         st.success('Summary document will be sent to your email shortly.')
 
-    # upload video to AssemblyAI
-    polling_endpoint = upload_to_AssemblyAI(uploaded_file)
+    def generate_summary():
+        # upload video to AssemblyAI
+        polling_endpoint = upload_to_AssemblyAI(uploaded_file)
 
-    status = 'submitted'
-    while status != "completed":
-        polling_response = requests.get(polling_endpoint, headers=headers)
-        status = polling_response.json()['status']
+        status = 'submitted'
+        while status != "completed":
+            polling_response = requests.get(polling_endpoint, headers=headers)
+            status = polling_response.json()['status']
 
-        if status == 'completed':
+            if status == 'completed':
 
-            # display chapter summaries
-            st.subheader('')
-            chapters = polling_response.json()['chapters']
-            chapters_df = pd.DataFrame(chapters)
+                # display chapter summaries
+                st.subheader('')
+                chapters = polling_response.json()['chapters']
+                chapters_df = pd.DataFrame(chapters)
 
-            # create text file with chapter summaries
+                # create text file with chapter summaries
 
-            summary_text = f"\n\nDate:{datetime.datetime.now().strftime('%d-%m-%Y')}\n"
-            summary_text += f"Number of attendees: {num_attendees}\n"
-            summary_text += f"Names of the speaker: {speaker_names}\n\n"
-            summary_text += f"Points discussed in the meet:\n\n"
+                summary_text = f"\n\nDate:{datetime.datetime.now().strftime('%d-%m-%Y')}\n"
+                summary_text += f"Number of attendees: {num_attendees}\n"
+                summary_text += f"Names of the speaker: {speaker_names}\n\n"
+                summary_text += f"Points discussed in the meet:\n\n"
 
-            for index, row in chapters_df.iterrows():
-                summary_text += f"-- {row['gist']}\n"
-                summary_text += f"   {row['summary']}\n\n"
+                for index, row in chapters_df.iterrows():
+                    summary_text += f"-- {row['gist']}\n"
+                    summary_text += f"   {row['summary']}\n\n"
 
-            doc = docx.Document()
-            heading = doc.add_heading('Minutes of the meet', level=1)
-            heading.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
-            doc.add_paragraph(summary_text)
-            doc_bytes = io.BytesIO()
-            doc.save(doc_bytes)
-            doc_bytes.seek(0)
+                doc = docx.Document()
+                heading = doc.add_heading('Minutes of the meet', level=1)
+                heading.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+                doc.add_paragraph(summary_text)
+                doc_bytes = io.BytesIO()
+                doc.save(doc_bytes)
+                doc_bytes.seek(0)
 
-            summary_gist = "Meeting summary points:\n"
-            for index, row in chapters_df.iterrows():
-                summary_gist += f"-- {row['gist']}\n"
+                summary_gist = "Meeting summary points:\n"
+                for index, row in chapters_df.iterrows():
+                    summary_gist += f"-- {row['gist']}\n"
 
-            # Replace the following variables with appropriate values
-            sender_email = "ashiqashervegar.2703@gmail.com"
-            sender_password = "wcgfigqopnfchfmw"
-            receiver_email = recipient_email
-            subject = "Summary Generated"
-            body = "Hello, Your summary is ready!!!!! \n\n" + summary_gist + "\n\nPlease find the attachment to go through the full summary"
+                # Replace the following variables with appropriate values
+                sender_email = "ashiqashervegar.2703@gmail.com"
+                sender_password = "wcgfigqopnfchfmw"
+                receiver_email = recipient_email
+                subject = "Summary Generated"
+                body = "Hello, Your summary is ready!!!!! \n\n" + summary_gist + "\n\nPlease find the attachment to go through the full summary"
 
-            # Attach the previously generated .doc file
+                # Attach the previously generated .doc file
 
-            attachment = MIMEApplication(doc_bytes.read(), _subtype='doc')
-            attachment.add_header('Content-Disposition', 'attachment', filename="meeting_summary.doc")
+                attachment = MIMEApplication(doc_bytes.read(), _subtype='doc')
+                attachment.add_header('Content-Disposition', 'attachment', filename="meeting_summary.doc")
 
-            # Create the email message
-            message = MIMEMultipart()
-            message['From'] = sender_email
-            message['To'] = receiver_email
-            message['Subject'] = subject
+                # Create the email message
+                message = MIMEMultipart()
+                message['From'] = sender_email
+                message['To'] = receiver_email
+                message['Subject'] = subject
 
-            message.attach(MIMEText(body))
-            message.attach(attachment)
+                message.attach(MIMEText(body))
+                message.attach(attachment)
 
-            # Send the email using the SMTP server of your email provider
-            with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-                smtp.ehlo()
-                smtp.starttls()
-                smtp.ehlo()
-                smtp.login(sender_email, sender_password)
-                smtp.sendmail(sender_email, receiver_email, message.as_string())
+                # Send the email using the SMTP server of your email provider
+                with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+                    smtp.ehlo()
+                    smtp.starttls()
+                    smtp.ehlo()
+                    smtp.login(sender_email, sender_password)
+                    smtp.sendmail(sender_email, receiver_email, message.as_string())
 
-                st.success("Email sent successfully!")
+    if st.button("Generate Summary"):
+        thread = threading.Thread(target=generate_summary)
+        thread.start()
